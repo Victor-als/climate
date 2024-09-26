123bd8cbebc990c598c304e57f1e951d4c2d5635
Victor-als <victor.ccx814@gmail.com>
2024-09-26 14:47:42 -0300

    feat: header styling adding title, icon and application logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Search } from 'lucide-react';
+import logo from '../assets/climate-logo.svg';
 
 
 interface IHeaderProps{
@@ -18,25 +19,28 @@ export function Header ({ onSearch }: IHeaderProps){
   };
   
   return(
-    <header className='flex w-screen items-center mb-6 justify-center p-4'>
-      {/* <span className='text-white'>Climate.Io</span> */}
-      <div className='flex mt-3 gap-4 items-center'>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Digite sua localidade"
-          className="p-4 w-[25rem] h-11 rounded-2xl bg-zinc-800 bg-opacity-75 text-zinc-400"
-        />
+    <header className='flex w-screen items-center mb-14 pt-10'>   
+       <div className='pl-24'>
+        <img src={logo}/>
+       </div>
+      
+        <div className='flex gap-4 m-auto'>
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="Digite sua localidade"
+            className="p-4 w-[25rem] h-11 rounded-2xl bg-zinc-800 bg-opacity-75 text-zinc-400"
+          />
 
-       
-          <button
-            onClick={handleSearch}
-            className="bg-zinc-800 h-11 p-4 text-zinc-400 rounded-full font-semibold"
-          >
-            <Search size={16}/>
-          </button>
-      </div>
+        
+            <button
+              onClick={handleSearch}
+              className="bg-zinc-800 h-11 p-4 text-zinc-400 rounded-full font-semibold"
+            >
+              <Search size={16}/>
+            </button>
+        </div>
     </header>
   )
 }
