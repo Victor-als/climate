19a89597381e92b726c45ee494bd59c1c8341445
Victor-als <victor.ccx814@gmail.com>
2024-10-22 16:55:53 -0300

    style: changes in 5-day forecasts

--- src/components/weather.tsx
+++ src/components/weather.tsx
@@ -80,40 +80,39 @@ export function Weather ({currentWeather, weatherData}: IWeatherProps){
           </div>
       )}
 
-      {weatherData && (
-        <div className='mt-8 w-full max-w-full'>
-          <h2 className="font-bold ml-4 flex items-center gap-4 text-zinc-50 mb-4">
-            <CalendarDays size={22}/>
-            Previsão dos próximos 5 dias
-          </h2>
-          <div className="bg-white bg-clip-padding backdrop-filter backdrop-blur-sm 
-           bg-opacity-10 w-full h-auto max-h-[28rem] shadow-shape rounded-3xl text-zinc-50 pt-4 px-4">
-            <div className="flex flex-col gap-2">
-              {
-                weatherData
-                && weatherData.list 
-                && weatherData.list.filter((_: any, index: number) => index % 8 === 0)
-                 .map((day: any, index: number) => (
-                <div key={index} className="p-2 w-full rounded-md">
-                  <div className="flex justify-between items-center">
-                    <div className="flex items-center">
-                      <img src={getIconUrl(day.weather[0].icon)} alt={day.weather[0].description} />
-                      <p className="text-lg flex">{formatTemperature(day.main.temp)}
-                        <span className="text-xs">°C</span>
-                      </p>
-                    </div>
-                      <p className="text-lg font-semibold">
-                      {formatDate(day.dt)}
-                      </p>
-                      <p className="text-md">{day.weather[0].description}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-        
-        )}
+{weatherData && (
+  <div className='mt-8 w-full max-w-full'>
+    <h2 className="font-bold ml-4 flex items-center gap-4 text-zinc-50 mb-4">
+      <CalendarDays size={22} />
+      Previsão dos próximos 5 dias
+    </h2>
+
+    <div className="bg-white bg-clip-padding backdrop-filter backdrop-blur-sm 
+         bg-opacity-10 w-full h-auto max-h-[28rem] shadow-shape rounded-3xl 
+         text-zinc-50 pt-4 px-4 overflow-auto">
+      <table className="table-auto w-full text-left text-zinc-50">
+        <tbody className="space-y-4">
+          {weatherData.list
+            .filter((_: any, index: number) => index % 8 === 0)
+            .map((day: any, index: number) => (
+              <tr key={index}>
+                <td className="px-4 py-4 flex items-center">
+                  <img 
+                    src={getIconUrl(day.weather[0].icon)} 
+                    alt={day.weather[0].description} 
+                    className="w-10 h-10"
+                  />
+                  {formatTemperature(day.main.temp)}°C
+                </td>
+                <td className="px-4 py-4">{formatDate(day.dt)}</td>
+                <td className="px-4 py-4 break-words">{day.weather[0].description}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+)}
     </div>
 
 
